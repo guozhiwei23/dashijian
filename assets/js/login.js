fae60f28032126a33f77a88e23fccb8725ef2225
guozhiwei <401062848@qq.com>
2020-10-30 14:35:10 +0800

    提交密码

---
+++ assets/js/login.js
@@ -0,0 +1,23 @@
+$(function () {
+    // 去注册
+    $('#link_reg').on('click', function () {
+        //隐藏登录框
+        $('.login-box').hide()
+        //显示注册框
+        $('.reg-box').show()
+    })
+    //去登陆
+    $('#link_login').on('click', function () {
+        //隐藏注册框
+        $('.reg-box').hide()
+        //显示登录框
+        $('.login-box').show()
+    })
+    //自定义校验规则
+    var form = layui.form
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ]
+    })
+})
